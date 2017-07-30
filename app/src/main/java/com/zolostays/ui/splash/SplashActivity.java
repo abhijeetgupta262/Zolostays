d402ab56b7433a4f9a29d005d08beba3c085a2fb
Abhijeet Gupta <abhijeet.gupta262@gmail.com>
2017-07-30 16:29:19 +0530

    Add splash view-model and navigator

--- app/src/main/java/com/zolostays/ui/splash/SplashActivity.java
+++ app/src/main/java/com/zolostays/ui/splash/SplashActivity.java
@@ -10,7 +10,7 @@ import com.zolostays.common.AppConstants;
  * This class/activity is responsible for the Splash of the application.
  * The Splash shows for the 5 sec and after that it redirect the app to the login screen.
  */
-public class SplashActivity extends AppCompatActivity
+public class SplashActivity extends AppCompatActivity implements SplashNavigator
 {
     /* ====================================== Interface ========================================= */
 
@@ -20,6 +20,8 @@ public class SplashActivity extends AppCompatActivity
 
     /* =================================== Class Variable ======================================= */
 
+    // Variable for the View Model
+    private SplashViewModel splashViewModel;
 
     /* ================================ Getter - Setter Method ================================== */
 
@@ -30,10 +32,35 @@ public class SplashActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Initialize the ViewModel
+        splashViewModel = new SplashViewModel(SplashActivity.this);
+        // Set Navigator for view model
+        splashViewModel.setNavigator(SplashActivity.this);
+        // Start view model
+        splashViewModel.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        splashViewModel.setBackPressed(true);
     }
 
     /* ============================= Implemented Interface Method =============================== */
 
+    @Override
+    public void goForLogin()
+    {
+        // Start the Login Screen
+
+    }
+
+    @Override
+    public void goForHome()
+    {
+        // Start the Home Screen
+    }
 
     /* ==================================== OnClick Methods ===================================== */
 
